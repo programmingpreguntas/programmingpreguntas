d8968de9b31072ae431891795a8ee1de561c4df2
Alex Chescheir <alexchescheir@Nancys-MacBook-Pro.local>
2016-05-17 16:25:39 -0400

    Created models for Question, Answer, and Usuario

--- programmingpreguntas/settings.py
+++ programmingpreguntas/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'preguntas.apps.PreguntasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
