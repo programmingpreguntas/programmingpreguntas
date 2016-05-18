780fc38509e32a4e9dff000d3dea4fe67e4ffcdb
gcrowder <gregory.j.crowder@gmail.com>
2016-05-18 15:22:18 -0400

    Fixed merge issues. New clean Master.

--- preguntas/views.py
+++ preguntas/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from .models import Question, Answer, Usuario
 from .serializers import QuestionSerializer, AnswerSerializer, UsuarioSerializer
+from django.contrib.auth import authenticate, login
+from django.template import loader
+from django.shortcuts import render_to_response
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 
 def profile(request, usuario_id):
@@ -35,3 +40,36 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     """
     queryset = Usuario.objects.all().order_by('name')
     serializer_class = UsuarioSerializer
+
+
+def index(request):
+
+    return render(request, 'preguntas_box/index.html')
+
+
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        print(request.POST)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+                # return redirect()
+
+            else:
+                state = "Your account is not active, please contact the site admin."
+                # return redirect()
+        else:
+            state = "Your username and/or password were incorrect."
+            # return redirect()
+
+    context = {'state':state, 'username': username}
+    return render(request, 'preguntas_box/auth.html', {'state':state, 'username': username})
