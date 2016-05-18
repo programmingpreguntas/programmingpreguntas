b1a94f8c80c1e8952f294e59230954a7412987ad
gcrowder <gregory.j.crowder@gmail.com>
2016-05-17 23:09:54 -0400

    Wired up admin. Got usuario profile template going.

---
+++ preguntas/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'preguntas'
+
+urlpatterns = [
+    url(r'^profile/(?P<usuario_id>[0-9]+)/$', views.profile, name='profile'),
+]
--- preguntas/views.py
+++ preguntas/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from .models import Question, Answer, Usuario
 from .serializers import QuestionSerializer, AnswerSerializer, UsuarioSerializer
+
+
+def profile(request, usuario_id):
+    usuario = get_object_or_404(Usuario, id=usuario_id)
+    question_set = Question.objects.filter(owner_id=usuario_id)
+    questions_answered_set = Answer.objects.filter(owner_id=usuario_id)
+    context = {'usuario': usuario, 'question_set': question_set,
+               'questions_answered_set': questions_answered_set}
+    return render(request, 'preguntas/profile.html', context)
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
