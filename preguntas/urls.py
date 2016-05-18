440a8f51f8d1869d711e7b0f5709e857edc684ab
gcrowder <gregory.j.crowder@gmail.com>
2016-05-18 10:12:15 -0400

    Added question template.

--- preguntas/urls.py
+++ preguntas/urls.py
@@ -6,4 +6,5 @@ app_name = 'preguntas'
 
 urlpatterns = [
     url(r'^profile/(?P<usuario_id>[0-9]+)/$', views.profile, name='profile'),
+    url(r'^questions/?P<question_id>[0-9]+)/$', views.question, name='question'),
 ]
